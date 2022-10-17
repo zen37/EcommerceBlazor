b0a34536d1ea0a8e1a157756464a78f9e74d9b39
zen37 <zen37>
2022-10-16 20:38:14 -0700

    bind properties

---
+++ EcommerceModels/Learn/DemoProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceModels.Learn
+{
+    public class DemoProduct
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+ 
+        public Boolean IsAvailable { get; set; }
+    }
+}
